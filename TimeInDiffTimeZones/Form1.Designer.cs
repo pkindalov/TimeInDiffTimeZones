307565cfb23a803d9e7bca0c8790bb062d816248
pkindalov <panayotkindalov@gmail.com>
2017-02-18 01:56:37 +0200

    Added main files of project

---
+++ TimeInDiffTimeZones/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace TimeInDiffTimeZones
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbZones = new System.Windows.Forms.ComboBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.btnCopyToClipboard = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cmbZones
+            // 
+            this.cmbZones.FormattingEnabled = true;
+            this.cmbZones.Location = new System.Drawing.Point(12, 12);
+            this.cmbZones.Name = "cmbZones";
+            this.cmbZones.Size = new System.Drawing.Size(247, 21);
+            this.cmbZones.Sorted = true;
+            this.cmbZones.TabIndex = 0;
+            this.cmbZones.Text = "Choose Time Zone";
+            this.cmbZones.SelectedIndexChanged += new System.EventHandler(this.cmbZones_SelectedIndexChanged);
+            this.cmbZones.Click += new System.EventHandler(this.cmbZones_Click);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResult.Location = new System.Drawing.Point(12, 53);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(247, 100);
+            this.txtResult.TabIndex = 1;
+            this.txtResult.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtResult.Click += new System.EventHandler(this.txtResult_Click);
+            this.txtResult.TextChanged += new System.EventHandler(this.txtResult_TextChanged);
+            // 
+            // btnCopyToClipboard
+            // 
+            this.btnCopyToClipboard.Location = new System.Drawing.Point(13, 160);
+            this.btnCopyToClipboard.Name = "btnCopyToClipboard";
+            this.btnCopyToClipboard.Size = new System.Drawing.Size(75, 23);
+            this.btnCopyToClipboard.TabIndex = 2;
+            this.btnCopyToClipboard.Text = "Copy";
+            this.btnCopyToClipboard.UseVisualStyleBackColor = true;
+            this.btnCopyToClipboard.Click += new System.EventHandler(this.btnCopyToClipboard_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(109, 160);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(0, 13);
+            this.lblStatus.TabIndex = 3;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(284, 201);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnCopyToClipboard);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.cmbZones);
+            this.Name = "frmMain";
+            this.Text = "Time in Different Time Zones";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbZones;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Button btnCopyToClipboard;
+        private System.Windows.Forms.Label lblStatus;
+    }
+}
+
---
+++ TimeInDiffTimeZones/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace TimeInDiffTimeZones
+{
+   
+    public partial class frmMain : Form
+    {
+        Dictionary<String, String> timeZones = new Dictionary<string, string>();
+        public frmMain()
+        {
+            InitializeComponent();
+
+
+            ReadOnlyCollection<TimeZoneInfo> zones = TimeZoneInfo.GetSystemTimeZones();
+
+            
+
+            foreach (TimeZoneInfo zone in zones)
+                timeZones.Add(zone.ToString(), zone.Id);
+            //cmbZones.Items.Add(zone.Id);
+
+
+            foreach (KeyValuePair<string, string> entry in timeZones)
+            {
+                // do something with entry.Value or entry.Key
+                cmbZones.Items.Add(entry.Key);
+            }
+
+        }
+
+        private void cmbZones_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+            String selectedTimeZone = cmbZones.Text;
+            if (timeZones.ContainsKey(selectedTimeZone))
+            {
+                selectedTimeZone = timeZones[selectedTimeZone];
+            }
+            
+
+
+            //txtResult.Text = selectedTimeZone;
+            ConvertToOtherTimeZone(selectedTimeZone);
+        }
+
+        private void ConvertToOtherTimeZone(string selectedTimeZone)
+        {
+            try
+            {
+                var time = TimeZoneInfo.ConvertTime(DateTime.Now,
+                    TimeZoneInfo.FindSystemTimeZoneById(selectedTimeZone));
+
+                txtResult.Text = time.ToString();
+            }
+            catch (Exception e)
+            {
+                txtResult.Text = e.Message;
+            }
+           
+        }
+
+        private void btnCopyToClipboard_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Clipboard.SetText(txtResult.Text);
+                lblStatus.Text = "Text copied successfully.";
+            }
+            catch (Exception ex)
+            {
+                lblStatus.Text = ex.Message;
+            }
+           
+        }
+
+        private void txtResult_TextChanged(object sender, EventArgs e)
+        {
+            lblStatus.Text = " ";
+        }
+
+        private void txtResult_Click(object sender, EventArgs e)
+        {
+            lblStatus.Text = " ";
+        }
+
+        private void cmbZones_Click(object sender, EventArgs e)
+        {
+            lblStatus.Text = " ";
+        }
+    }
+}
